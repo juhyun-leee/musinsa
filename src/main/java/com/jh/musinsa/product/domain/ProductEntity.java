3d301f154ce48bc9b978edca6b36d097064f05b4
juhyun-leee <dev.juhyun@gmail.com>
2024-08-29 17:02:20 +0900

    [구현 1] refactor: subquery 수행에 필요한 (category_id, price) 에 대한 복합 인덱스 설정

--- src/main/java/com/jh/musinsa/product/domain/ProductEntity.java
+++ src/main/java/com/jh/musinsa/product/domain/ProductEntity.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -16,7 +17,12 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "product")
+@Table(
+        name = "product",
+        indexes = {
+                @Index(name = "idx_category_price", columnList = "category_id, price")
+        }
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductEntity extends BaseEntity {
 
